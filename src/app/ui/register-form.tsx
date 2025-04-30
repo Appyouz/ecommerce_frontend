1b784591bf0cc70a595b88f3c6aaca990e6b03fe
appyouz <dikpalkhatri7@gmail.com>
2025-04-30 17:57:15 +0545

    Refactor: userState for formData

--- src/app/ui/register-form.tsx
+++ src/app/ui/register-form.tsx
@@ -1,23 +1,48 @@
+'use client';
+import { useState } from "react";
+
 // Component: Register
+
 export default function RegisterForm() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password1: '',
+    password2: ''
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Form submitted:', formData)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">username</label>
-        <input type="username" id="username" />
+        <input name="username" value={formData.username} onChange={handleChange} type="text" id="username" />
       </div>
 
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input name="email" value={formData.email} onChange={handleChange} type="email" id="email" />
       </div>
+
       <div>
         <label htmlFor="password1">Password</label>
-        <input type="password1" id="password1" />
+        <input name="password1" value={formData.password1} onChange={handleChange} type="password1" id="password1" />
       </div>
       <div>
         <label htmlFor="password2">Confirm password</label>
-        <input type="password2" id="password2" />
+        <input name="password2" value={formData.password2} onChange={handleChange} type="password2" id="password2" />
       </div>
       <button type="submit">Register</button>
     </form>
