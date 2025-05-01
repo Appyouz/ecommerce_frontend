4ff7a7f2fbf559f935c9bd05f0fd1ece8020f019
appyouz <dikpalkhatri7@gmail.com>
2025-05-01 12:01:47 +0545

    login route created

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/app/ui/login-form';
+
+// Register route
+export default function LoginPage() {
+  return (
+    <div><h1>Login</h1>
+      <LoginForm />
+    </div>
+
+  )
+}
+
+
