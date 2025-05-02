1d377d09427d21e957c6d6c23160b6a6d3c90826
appyouz <dikpalkhatri7@gmail.com>
2025-05-02 21:04:32 +0545

    Imported and rednered Header in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import { AuthProvider } from "./context/auth-content"
+import Header from "./ui/headers"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
         <main className="container mx-auto px-4 py-8">
           <AuthProvider>
+            <Header />
             {children}
           </AuthProvider>
         </main>
