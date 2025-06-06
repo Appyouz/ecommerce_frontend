65036744b315a3e878d2e2247392aa0edca534d0
appyouz <dikpalkhatri7@gmail.com>
2025-06-06 22:48:20 +0545

    REFACTOR: moved to old cart type

--- src/types/cart-item.ts
+++ src/types/cart-item.ts
@@ -1,19 +1,29 @@
 import { Product } from "./product";
-
 export type CartItem = {
   id: number;
-  cart: number;
   product: Product;
   quantity: number;
-  created_at: string;
-  updated_at: string;
 };
-
 export type Cart = {
   id: number;
-  cart: number;
-  product: Product;
-  quantity: number;
-  created_at: string;
-  updated_at: string;
+  items: CartItem[];
+  total_price: string;
 };
+
+// export type CartItem = {
+//   id: number;
+//   cart: number;
+//   product: Product;
+//   quantity: number;
+//   created_at: string;
+//   updated_at: string;
+// };
+//
+// export type Cart = {
+//   id: number;
+//   cart: number;
+//   product: Product;
+//   quantity: number;
+//   created_at: string;
+//   updated_at: string;
+// };
